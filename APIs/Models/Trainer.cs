c08401cf847ced5596f24112850bb61d2db94e52
Ezz Saeed <ezzsaid299@gmail.com>
2025-03-18 00:23:02 +0200

    Adding project models

---
+++ APIs/Models/Trainer.cs
@@ -0,0 +1,9 @@
+﻿namespace APIs.Models
+{
+    public class Trainer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public ICollection<Course>? Courses { get; set; }
+    }
+}
